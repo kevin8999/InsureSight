8b096534f62dcea7269d8bd99af9943e3178e9bb
Kevin D <76125913+Vomet@users.noreply.github.com>
2024-12-17 12:14:51 -0500

    Add ability to upload base64 data to IMGBB via its API

--- backend/image_uploader.py
+++ backend/image_uploader.py
@@ -16,24 +16,29 @@ def image_to_base64(image_file):
     b64_string = b64_bytes.decode()
     return b64_string
 
-def upload_image(API_KEY, b64_string):
-    # Uploads image to the IMGBB service using base64
-    pass
+def upload_image(API_KEY, b64_string, expiration_time=600):
+    # Uploads image to the IMGBB service using base64 data. IMGBB requires base64 data when uploading from its API
+    command = f"curl --location --request POST 'https://api.imgbb.com/1/upload?expiration={str(expiration_time)}&key={API_KEY}' --form 'image={b64_string}'"
+    os.system(command)
 
 def main():
     load_dotenv()
     IMGBB_API_KEY = os.getenv("IMGBB_API_KEY")
 
-    image_paths = get_image_paths()
-    b64_strings = []
+    image_paths = get_image_paths(file_extension='.jpg')
 
-    # Convert each image found in directory to base64
+    # Convert each image found in ./images to base64
+    b64_strings = []
     for image_path in image_paths:
         image_file = open(image_path, 'rb')
         b64_str = image_to_base64(image_file)
 
         b64_strings.append(b64_str)
     
+    # Upload to IMGBB hosting service using base64 strings
+    for i, string in enumerate(b64_strings):
+        print(f"Uploading image {i+1} of {len(b64_strings)}")
+        upload_image(IMGBB_API_KEY, string)
 
 if __name__ == '__main__':
     main()
