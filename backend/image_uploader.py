f40b06cddef7d24bc3cda0671c6873062b2ad2a2
Kevin D <76125913+Vomet@users.noreply.github.com>
2024-12-17 12:41:29 -0500

    Add ability to get URL of image after upload

--- backend/image_uploader.py
+++ backend/image_uploader.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv, dotenv_values
 import os
 import glob
 import base64
+import subprocess
+import json
 
 def get_image_paths(file_extension='.jpg', directory='./images'):
     image_list = glob.glob(os.path.join(directory, f"*{file_extension}"))
@@ -16,10 +18,19 @@ def image_to_base64(image_file):
     b64_string = b64_bytes.decode()
     return b64_string
 
-def upload_image(API_KEY, b64_string, expiration_time=600):
+def upload_image(API_KEY, b64_string, expiration_time=60):
     # Uploads image to the IMGBB service using base64 data. IMGBB requires base64 data when uploading from its API
     command = f"curl --location --request POST 'https://api.imgbb.com/1/upload?expiration={str(expiration_time)}&key={API_KEY}' --form 'image={b64_string}'"
-    os.system(command)
+    
+    data = subprocess.check_output(command, shell=True, text=True)
+
+    # Clean data to make it easier to parse the URL later
+    data = data.replace("\n", "")
+    data = data.replace("\\", "")
+    data = data.replace("  ", "")
+
+    # Return JSON data from IMGBB
+    return json.loads(data)
 
 def main():
     load_dotenv()
@@ -35,10 +46,18 @@ def main():
 
         b64_strings.append(b64_str)
     
+    img_urls = []
     # Upload to IMGBB hosting service using base64 strings
     for i, string in enumerate(b64_strings):
         print(f"Uploading image {i+1} of {len(b64_strings)}")
-        upload_image(IMGBB_API_KEY, string)
+        output = upload_image(IMGBB_API_KEY, string)
+
+        # If image was successfully uploaded, append its display URL to img_urls
+        if output["success"] == True:
+            url = output["data"]["display_url"]
+            img_urls.append(url)
+    
+    print(img_urls)
 
 if __name__ == '__main__':
     main()
